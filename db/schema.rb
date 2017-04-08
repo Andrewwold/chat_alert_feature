c4bb2c3ce8d629220906acc055bf7194f6c2bc34
Andrew <goldsabre@gmail.com>
2017-04-07 23:16:44 -0600

    added STI for admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117180039) do
+ActiveRecord::Schema.define(version: 20170408050642) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20161117180039) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
